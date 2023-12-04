1c87f69e83408dc189f9fa281f0f22bcef809df8
Johan <johan.hm@hotmail.dk>
2023-12-04 12:37:51 +0100

    hej

--- TaghelpersAndOverposting/Models/UsermodelAlsoSafe.cs
+++ TaghelpersAndOverposting/Models/UsermodelAlsoSafe.cs
@@ -1,6 +1,18 @@
-﻿namespace TaghelpersAndOverposting.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace TaghelpersAndOverposting.Models
 {
     public class UsermodelAlsoSafe
     {
+        [MaxLength(200)]
+        [Display(Name = "Fullname")]
+        [Required]
+        public string Name { get; set; }
+    }
+
+    public class UsermodelBlsoSafe : UsermodelAlsoSafe
+    {
+        public bool isAdmin { get; set; }
     }
 }
