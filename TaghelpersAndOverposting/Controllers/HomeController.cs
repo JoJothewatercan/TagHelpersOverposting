fbd7fa817744e1716ef1edca56143ce2404f0988
Rothkepchen <130970150+Rothkepchen@users.noreply.github.com>
2023-12-04 11:38:46 +0100

    Tag helper eksempel

--- TaghelpersAndOverposting/Controllers/HomeController.cs
+++ TaghelpersAndOverposting/Controllers/HomeController.cs
@@ -22,6 +22,15 @@ namespace TaghelpersAndOverposting.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult AboutTwo()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
